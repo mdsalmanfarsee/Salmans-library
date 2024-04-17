a044f2e514b79c0e585572903f28d5925fa7a919
mdsalmanfarsee <far90986@gmail.com>
2024-04-17 12:07:30 +0530

    author added

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="app">
       <h1>Movie hub</h1>
-      <h3>--By Salman</h3>
+      <h3 className="author ">--By Salman</h3>
       <div className="search">
         <input
           placeholder="Search for movies"
